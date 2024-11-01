40277dedc4a8ce32b1b29ce25be8935c1a6499a0
Benjamin Golding <ben_w_golding@hotmail.com>
2024-11-01 10:36:09 +0100

    Added cpp source file template

---
+++ cpp_templates/class_source.cpp
@@ -0,0 +1,28 @@
+#include "Nameplaceholder.hpp"
+
+//  ==========| CONSTRUCTORS |==========
+Nameplaceholder:Nameplaceholder()
+{
+    //  DEFAULT
+}
+
+Nameplaceholder::Nameplaceholder(const Nameplaceholder &other)
+{
+    //  DEFAULT
+}
+
+//  ===========| DESTRUCTOR |===========
+Nameplaceholder::~Nameplaceholder()
+{
+    //  DEFAULT
+}
+
+//  =======| OPERATOR OVERLOADS |=======
+Nameplaceholder &Nameplaceholder::operator=(const Nameplaceholder &rhs)
+{
+    //  DEFAULT
+}
+
+//  ============| METHODS |=============
+
+//  ========| VIRTUAL METHODS |=========
